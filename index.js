ac5d9af0c2b58c0c399246029ca4dc92ddefe659
megakmpers <megakmpers@gmail.com>
2018-04-17 15:40:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = 'mongodb://easylivin:24206492@ds119486.mlab.com:19486/tutorial-app';
+var databaseUri = '';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'com.razeware.ParseTutorial',
-  masterKey: process.env.MASTER_KEY || '24206492', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || '',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
